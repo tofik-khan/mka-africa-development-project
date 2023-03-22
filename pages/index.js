a005436d6cab56abe828314e9ef861f1f4a936d2
Tofik Khan <tofik.khan@mkausa.org>
2023-03-22 01:09:13 -0700

    Create and Add Navigation component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Hero from "../partials/homepage/hero";
+import { Navigation } from "../partials/Nav";
 
 export default function Page() {
   return (
     <>
-      <h1>This is homepage</h1>
+      <Navigation />
       <Hero />
     </>
   );
---
+++ partials/Nav.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import Hamburger from "hamburger-react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import logomark from "/public/images/adi-logomark.webp";
+import Button from "../components/Button";
+
+const StyledNavBar = styled(Navbar)`
+  background-color: #042c3d;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const StyledSpacer = styled.div`
+  margin-bottom: 100px;
+`;
+
+export const Navigation = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <StyledNavBar fixed="top" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <Image src={logomark} width={250} />
+          </Navbar.Brand>
+          <Navbar.Toggle>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </Navbar.Toggle>
+          <Navbar.Collapse className="justify-content-end align-items-center">
+            <Nav>
+              <Nav.Link
+                href="https://www.classy.org/give/475449/#!/donation/checkout"
+                target="_blank"
+                rel="noreferrer"
+                className="align-items-center"
+              >
+                Donate
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </StyledNavBar>
+      <StyledSpacer />
+    </>
+  );
+};
