380f83f9cf158cf05d7d41847fef6cfb22baa76c
Tofik Khan <tofik.khan@mkausa.org>
2023-03-21 21:01:20 -0700

    Include hero in homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import Hero from "../partials/homepage/hero";
+
 export default function Page() {
-  return <h1>This is homepage</h1>;
+  return (
+    <>
+      <h1>This is homepage</h1>
+      <Hero />
+    </>
+  );
 }
